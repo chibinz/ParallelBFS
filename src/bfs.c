6988e142392105a3854e6660755541e57189f22e
Chibin Zhang <chibinzhang@hotmail.com>
2021-05-14 16:35:45 +0800

    Implement bfs_result

--- src/bfs.c
+++ src/bfs.c
@@ -9,14 +9,26 @@
 
 /// Output Format
 /// <id> <distance> <parent>
+typedef struct {
+  usize *parent;
+  usize *distance;
+} bfs_result;
+
+void write_bfs_result(bfs_result r, usize n, FILE *f) {
+  for (usize i = 0; i < n; i += 1) {
+    fprintf(f, "%lu\t%lu\t%lu\n", i, r.distance[i], r.parent[i]);
+  }
+}
 
 /// Serial breadth first search on csr matrix
-void bfs(csr *adj, usize src) {
+bfs_result bfs(csr *adj, usize src) {
   // Adjacency matrix should be square matrix
   assert(adj->m == adj->n);
 
-  queue *q = queue_new(adj->m);
-  bitmap *b = bitmap_new(adj->m);
+  queue *q = queue_new(adj->n);
+  bitmap *b = bitmap_new(adj->n);
+  usize *parent = malloc(sizeof(usize) * adj->n);
+  usize *distance = malloc(sizeof(usize) * adj->n);
 
   queue_push(q, src);
   while (!queue_empty(q)) {
@@ -24,11 +36,14 @@ void bfs(csr *adj, usize src) {
     for (usize j = csr_row_begin(adj, e); j < csr_row_end(adj, e); j += 1) {
       if (bitmap_test_set(b, adj->c[j])) {
         queue_push(q, adj->c[j]);
-        // printf("%lu\n", adj->c[j]);
+        parent[adj->c[j]] = e;
+        distance[adj->c[j]] = distance[e] + 1;
       }
     }
   }
 
   queue_free(q);
   bitmap_free(b);
+
+  return (bfs_result){parent, distance};
 }
